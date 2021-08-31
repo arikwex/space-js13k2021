51606cbf778ab3027f29eb374e83a2c8549d7865
Ariel Wexler <ariel@markforged.com>
2021-08-30 21:08:37 -0400

    minimap at top

--- src/engine.js
+++ src/engine.js
@@ -15,7 +15,7 @@ export default function Engine() {
     if (tickAnim > 1) {
       tickAnim--;
       currentTick++;
-      console.log(currentTick);
+      // TODO: handle round over
     }
   };
 
@@ -34,6 +34,31 @@ export default function Engine() {
     // Scale of the art
     const s = Math.min(ph, w/8) * 0.3;
 
+    // Minimap
+    // Progress path
+    var pr=th/2*0.7;
+    ctx.fillStyle = '#446';
+    ctx.fillRect(15 + pw, th/2, w - 30 - 2*pw, 1);
+    // Source planet
+    ctx.fillStyle = '#446';
+    ctx.beginPath();
+    ctx.arc(15 + pr, th/2, pr, 0, 6.28);
+    ctx.fill();
+    // Dest planet
+    ctx.fillStyle = '#446';
+    ctx.beginPath();
+    ctx.arc(w - 15 - pr, th/2, pr, 0, 6.28);
+    ctx.fill();
+    // Progress ship
+    ctx.fillStyle = '#fff';
+    var px = 15 + pw + (w - 30 - 2*pw) * ((currentTick + tickAnim)/totalTicks);
+    ctx.beginPath();
+    ctx.moveTo(px+pw/3, th/2);
+    ctx.lineTo(px-pw/3, th/2-pw/3);
+    ctx.lineTo(px-pw/3, th/2+pw/3);
+    ctx.closePath();
+    ctx.fill();
+
     // Draw the lanes
     ctx.fillStyle = '#f33'; ctx.fillRect(15, th, pw, ph);
     ctx.fillStyle = '#3f3'; ctx.fillRect(15, mh, pw, ph);
@@ -47,10 +72,13 @@ export default function Engine() {
     ctx.fillRect(15, bh, w-30, 1);
     ctx.fillRect(15, bh+ph, w-30, 1);
 
-    // Draw the ship
+    // Draw the main ship
     ctx.save();
+
+    // base ship position
     ctx.translate(50 + pw + s, mh + ph/2);
 
+    // ship flames
     for (let i = 5; i >= 0; i--) {
       if (i < 1) {
         ctx.fillStyle = '#fd6';
@@ -64,9 +92,10 @@ export default function Engine() {
       ctx.arc(-s - s * i * 0.1, Math.sin(-anim*40+i)*i*s/30, (s * spot) / (1.5 + i*0.3), 0, 6.29);
       ctx.fill();
     }
-    ctx.fillStyle = '#fff';
-    ctx.translate(Math.sin(anim*30) * s/60, Math.sin(anim*35) * s/40);
 
+    // ship body + shaking
+    ctx.translate(Math.sin(anim*30) * s/60, Math.sin(anim*35) * s/40);
+    ctx.fillStyle = '#fff';
     ctx.beginPath();
     ctx.moveTo(s, 0);
     ctx.lineTo(-s*0.65,-s*0.8);
