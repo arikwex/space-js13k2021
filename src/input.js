f283f5836276c8165665393bf5cffdc24d06a4e1
Ariel Wexler <ariel@markforged.com>
2021-09-01 01:01:31 -0400

    Once upon an input

--- src/input.js
+++ src/input.js
@@ -1,27 +1,25 @@
 import bus from './bus.js';
-bus.on('tap', console.log);
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  document.body.addEventListener('touchstart', (evt) => {
-    evt.preventDefault();
-    evt.stopPropagation();
-    bus.emit('tap', { x: evt.touches[0].clientX, y: evt.touches[0].clientY });
-  });
-  document.body.addEventListener('touchend', (evt) => {
-    evt.preventDefault();
-    evt.stopPropagation();
-    bus.emit('move', { x: -1, y: -1});
-  });
-} else {
-  document.body.addEventListener('mousedown', (evt) => {
-    evt.preventDefault();
-    evt.stopPropagation();
-    bus.emit('tap', { x: evt.x, y: evt.y });
-  });
-}
-document.body.addEventListener('mousemove', (evt) => {
+isMobile = ()=>/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+window.addEventListener('touchstart', (evt) => {
+  evt.stopPropagation();
+  bus.emit('tap', { x: evt.touches[0].clientX, y: evt.touches[0].clientY });
+});
+
+window.addEventListener('mousedown', (evt) => {
   evt.preventDefault();
   evt.stopPropagation();
-  bus.emit('move', { x: evt.x, y: evt.y });
+  bus.emit('tap', { x: evt.x, y: evt.y });
+});
+
+window.addEventListener('mousemove', (evt) => {
+  evt.preventDefault();
+  evt.stopPropagation();
+  if (isMobile()) {
+    bus.emit('move', { x: -1, y: -1 });
+  } else {
+    bus.emit('move', { x: evt.x, y: evt.y });
+  }
 });
 
