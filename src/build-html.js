893f6122d6457a19dab9e6afa4c42378beacbe84
Ariel Wexler <ariel@markforged.com>
2021-09-09 00:52:39 -0400

    Set page title

--- src/build-html.js
+++ src/build-html.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 var html = '';
-html += '<html>';
+html += '<html><title>SHUTTLEDECK</title>';
 html += '<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" sizes="64x64"/>';
 html += '<meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0\">';
 html += '<style>canvas{width:100%;height:100%;}body{margin:0;background:#000;}</style>';
